e9fcfc62376e3ad92be63959b36f85799ea13750
Cesar Oyarzun <cesar.oyarzun@scala.com>
2015-04-02 09:55:54 -0400

    remove www

--- admin/platforms/ios/AdminApp/Plugins/com.scala.BeaconController/BeaconController.h
+++
@@ -1,8 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-#import "ESTBeaconController.h"
-
-@interface BeaconController : CDVPlugin
-
--(BeaconController*) pluginInitialize;
-
-@end
--- admin/plugins/com.scala.BeaconController/plugin/src/ios/BeaconController.h
+++
@@ -1,9 +0,0 @@
-
-#import <Cordova/CDV.h>
-#import "ESTBeaconController.h"
-
-@interface BeaconController : CDVPlugin
-
--(BeaconController*) pluginInitialize;
-
-@end
